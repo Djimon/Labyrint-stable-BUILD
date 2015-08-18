76739e90391ae5a1be77e3159e9434f4ec70d5d9
djimon <djimon@gmx.de>
2015-08-18 17:28:12 +0200

    noch nciht ganz stable KI-test

--- IcyMazeRunner/Klassen/Gameplay/Enemy.cs
+++ IcyMazeRunner/Klassen/Gameplay/Enemy.cs
@@ -8,22 +8,28 @@ using System.Threading.Tasks;
 
 namespace IcyMazeRunner.Klassen.Gameplay
 {
-    class Enemy : GameObject
+    class Enemy
     {
         Vector2f position;
         Sprite sprite;
+        Map mMap;
+        float F_speed = 0.5f;
 
-        public Enemy(Vector2f _position, string texturePath)
+        public Enemy(Vector2f _position, string texturePath, Map map)
         {
             position = _position;
-
+            mMap = map;
             Texture txEnemy = new Texture(texturePath);
             sprite = new Sprite(txEnemy);
         }
 
-        public void move() //andere Bewegungsmuster (e.g. Pathfinder)
+        public void move(Map mmmap,GameTime time) //andere Bewegungsmuster (e.g. Pathfinder)
         {
-        
+            F_speed = 0.4f * time.ElapsedTime.Milliseconds;
+            if (mmmap.iswalkable((sprite), new Vector2f(-F_speed,0)))
+            {
+                // your code here
+            }
         }
 
         public void move(Vector2f PlayerPosition)  //direkter weg zum player
@@ -33,15 +39,15 @@ namespace IcyMazeRunner.Klassen.Gameplay
             position += direction / (length * 5);
         }
 
-        public override void update(GameTime gametime)
+        public void update(GameTime Gtime)
         {
-            return;
+            move(this.mMap, Gtime);
         }
 
-        //public override void draw(RenderWindow win)
-        //{
-        //    sprite.Position = position;
-        //    win.Draw(sprite);
-        //}
+        public void draw(RenderWindow win)
+        {
+            sprite.Position = position;
+            win.Draw(sprite);
+        }
     }
 }
