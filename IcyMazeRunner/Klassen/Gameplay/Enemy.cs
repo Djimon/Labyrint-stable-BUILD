7b4d77db8e7298ccfe558331c9648276401848c7
Mortality042 <schiller1996@arcor.de>
2015-08-19 17:46:14 +0200

    Enemy.cs- move Methode überarbeitet

--- IcyMazeRunner/Klassen/Gameplay/Enemy.cs
+++ IcyMazeRunner/Klassen/Gameplay/Enemy.cs
@@ -34,76 +34,102 @@ namespace IcyMazeRunner.Klassen.Gameplay
              //1- right
              //2- bot  
              //3- left
+
+            //Vektor- Position des Players
             Vector2f playerDistance = new Vector2f(player.getXPosition(),player.getYPosition());
 
-            Vector2f tmp = new Vector2f(position.X, position.Y+1);
-            float testTop = calc.getDistance(playerDistance, tmp);
-
-            tmp = new Vector2f(position.X+1, position.Y );
-            float testRight = calc.getDistance(playerDistance, tmp);
-
-            tmp = new Vector2f(position.X, position.Y - 1);
-            float testBot = calc.getDistance(playerDistance, tmp);
-
-            tmp = new Vector2f(position.X-1, position.Y);
-            float testLeft = calc.getDistance(playerDistance, tmp);
-
-            enemy_move top = new enemy_move(0, testTop);
-            enemy_move right = new enemy_move(1, testRight);
-            enemy_move bot = new enemy_move(2, testBot);
-            enemy_move left = new enemy_move(3, testLeft);
-           
-            enemy_move[] choose = new enemy_move[4];
-            choose[0] = top;
-            choose[1] = right;
-            choose[2] = bot;
-            choose[3] = left;
-
-            //Insertionsort auf das enemy_move array über die Distances
-            for (int i = 0; i < choose.Length - 1; i++)
+            //Vektoren ausgehend vom Enemy nach oben unten rechts und links -> Distanz zum Player
+            Vector2f tmp = new Vector2f(position.X, position.Y-100);
+            if (mmmap.iswalkable((sprite), new Vector2f(position.X, position.Y))) { }
+            else
             {
-                int j = i + 1;
-                while (j > 0)
+                float testTop = calc.getDistance(playerDistance, tmp);
+
+                tmp = new Vector2f(position.X + 100, position.Y);
+                float testRight = calc.getDistance(playerDistance, tmp);
+
+                tmp = new Vector2f(position.X, position.Y + 100);
+                float testBot = calc.getDistance(playerDistance, tmp);
+
+                tmp = new Vector2f(position.X - 100, position.Y);
+                float testLeft = calc.getDistance(playerDistance, tmp);
+
+                //Zuweisung der Distanzen und (0,1,2,3) kodierter Richtung
+                enemy_move top = new enemy_move(0, testTop);
+                enemy_move right = new enemy_move(1, testRight);
+                enemy_move bot = new enemy_move(2, testBot);
+                enemy_move left = new enemy_move(3, testLeft);
+
+                enemy_move[] choose = new enemy_move[4];
+                choose[0] = top;
+                choose[1] = right;
+                choose[2] = bot;
+                choose[3] = left;
+
+                //Insertionsort auf das enemy_move Array über die Distanzen
+                for (int i = 0; i < choose.Length - 1; i++)
                 {
-                    if (choose[j - 1].distance > choose[j].distance)
+                    int j = i + 1;
+                    while (j > 0)
                     {
-                        float temp = choose[j - 1].distance;
-                        choose[j - 1].distance = choose[j].distance;
-                        choose[j].distance = temp;
+                        if (choose[j - 1].distance > choose[j].distance)
+                        {
+                            float temp = choose[j - 1].distance;
+                            choose[j - 1].distance = choose[j].distance;
+                            choose[j].distance = temp;
+                        }
+                        j--;
                     }
-                    j--;
+                }
+
+                int go_tmp, go;
+                go = 4;
+
+                go_tmp = choose[3].direction;
+                switch (go_tmp)
+                {
+                    case 0: if (mMap.iswalkable((sprite), new Vector2f(0, -F_speed))) { go = 0; } break;
+                    case 1: if (mMap.iswalkable((sprite), new Vector2f(F_speed, 0))) { go = 1; } break;
+                    case 2: if (mMap.iswalkable((sprite), new Vector2f(0, F_speed))) { go = 2; } break;
+                    case 3: if (mMap.iswalkable((sprite), new Vector2f(-F_speed, 0))) { go = 3; } break;
+                }
+
+                go_tmp = choose[2].direction;
+                switch (go_tmp)
+                {
+                    case 0: if (mMap.iswalkable((sprite), new Vector2f(0, -F_speed))) { go = 0; } break;
+                    case 1: if (mMap.iswalkable((sprite), new Vector2f(F_speed, 0))) { go = 1; } break;
+                    case 2: if (mMap.iswalkable((sprite), new Vector2f(0, F_speed))) { go = 2; } break;
+                    case 3: if (mMap.iswalkable((sprite), new Vector2f(-F_speed, 0))) { go = 3; } break;
+                }
+
+                go_tmp = choose[1].direction;
+                switch (go_tmp)
+                {
+                    case 0: if (mMap.iswalkable((sprite), new Vector2f(0, -F_speed))) { go = 0; } break;
+                    case 1: if (mMap.iswalkable((sprite), new Vector2f(F_speed, 0))) { go = 1; } break;
+                    case 2: if (mMap.iswalkable((sprite), new Vector2f(0, F_speed))) { go = 2; } break;
+                    case 3: if (mMap.iswalkable((sprite), new Vector2f(-F_speed, 0))) { go = 3; } break;
+                }
+
+                go_tmp = choose[0].direction;
+                switch (go_tmp)
+                {
+                    case 0: if (mMap.iswalkable((sprite), new Vector2f(0, -F_speed))) { go = 0; } break;
+                    case 1: if (mMap.iswalkable((sprite), new Vector2f(F_speed, 0))) { go = 1; } break;
+                    case 2: if (mMap.iswalkable((sprite), new Vector2f(0, F_speed))) { go = 2; } break;
+                    case 3: if (mMap.iswalkable((sprite), new Vector2f(-F_speed, 0))) { go = 3; } break;
+                }
+
+                switch (go)
+                {
+                    case 0: position = new Vector2f(position.X, position.Y - 1); break;
+                    case 1: position = new Vector2f(position.X + 1, position.Y); break;
+                    case 2: position = new Vector2f(position.X, position.Y + 1); break;
+                    case 3: position = new Vector2f(position.X - 1, position.Y); break;
+                    case 4: break;
                 }
             }
-
-            int go = 0;
-            if (mmmap.iswalkable((sprite), new Vector2f(F_speed, 0)))
-            {
-                go = choose[3].direction;
-            }
-            if (mmmap.iswalkable((sprite), new Vector2f(F_speed, 0)))
-            {
-                go = choose[2].direction;
-            } 
-            if (mmmap.iswalkable((sprite), new Vector2f(F_speed, 0)))
-            {
-                go = choose[1].direction;
-            } 
-            if (mmmap.iswalkable((sprite), new Vector2f(F_speed, 0)))
-            {
-                go = choose[0].direction;
-            }
-            
-             
-            
-
-             switch (go)
-             {
-                 case 0: position = new Vector2f(position.X, position.Y + 1); break;
-                 case 1: position = new Vector2f(position.X+1, position.Y ); break;
-                 case 2: position = new Vector2f(position.X, position.Y - 1); break;
-                 case 3: position = new Vector2f(position.X - 1, position.Y); break;
-             }
-            
         }
 
         public void move(Vector2f PlayerPosition)  //direkter weg zum player
