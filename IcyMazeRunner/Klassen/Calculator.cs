8517222d8dc2fadca5bdaf2aeb0f1fac2d16e92f
Djimon <djimon@gmx.de>
2015-08-20 08:55:19 +0200

    Update Calculator.cs

--- IcyMazeRunner/Klassen/Calculator.cs
+++ IcyMazeRunner/Klassen/Calculator.cs
@@ -14,13 +14,19 @@ namespace IcyMazeRunner.Klassen
         {
 
         }
-
+        
+        ///<summary>
+        ///gibt die euklidische Distanz zwischen Vekto1 und Vektor2 zurück.
+        ///</summary>
         public float getDistance(Vector2f VectorOne, Vector2f VectorTwo)
         {
             float distance;
             return distance = (float)Math.Sqrt(Math.Pow(Math.Abs(VectorOne.X-VectorTwo.X),2) + Math.Pow(Math.Abs(VectorOne.Y-VectorTwo.Y),2));
         }
-
+        
+        ///<summary>
+        /// Gibt Winkel (in Grad) zwishcen x-Achse und position-Verktor zurück.
+        ///</summary>
         public float getWinkel(Vector2f position) //zur x-Achse
         {
             float x = position.X;
